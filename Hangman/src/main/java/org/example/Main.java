b354c19413e496a4626bfde92a8f4e4e7a26d4bb
Petya Ateva <petya.ateva@cleverpine.com>
2025-04-06 17:09:01 +0300

    updates to the runner class

--- Hangman/src/main/java/org/example/Main.java
+++ Hangman/src/main/java/org/example/Main.java
@@ -1,30 +1,10 @@
 package org.example;
 
-import java.util.Scanner;
-
-import static org.example.utils.Constants.PLAY_AGAIN;
-import static org.example.utils.Constants.PLAY_AGAIN_YES;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Runner runner = new Runner();
-        boolean playAgain;
-        Scanner scanner = new Scanner(System.in);
-
-        do {
-            runner.hangmanRunner();
-            playAgain = playAgain(scanner);
-
-        } while (playAgain);
-
-        scanner.close();
-    }
-
-    private static boolean playAgain(Scanner scanner) {
-        System.out.println(PLAY_AGAIN);
-        String response = scanner.nextLine();
-        return response.equalsIgnoreCase(PLAY_AGAIN_YES);
+        runner.play(runner::hangmanRunner);
     }
 }
--- Hangman/src/main/java/org/example/Runner.java
+++ Hangman/src/main/java/org/example/Runner.java
@@ -4,11 +4,24 @@ import org.example.games.Hangman;
 import org.example.utils.HangmanState;
 
 import java.util.Scanner;
+import java.util.function.Function;
 
 import static org.example.utils.Constants.GET_ANOTHER_WORD;
 import static org.example.utils.Constants.GUESS_A_LETTER;
+import static org.example.utils.Constants.PLAY_AGAIN;
+import static org.example.utils.Constants.PLAY_AGAIN_YES;
 
 public class Runner {
+    public void play(Runnable function) {
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            function.run();
+        } while (playAgainFun(scanner));
+
+        scanner.close();
+    }
+
     public void hangmanRunner() {
         Hangman hangman = new Hangman();
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +30,7 @@ public class Runner {
             System.out.println(GUESS_A_LETTER);
             String guess = scanner.next();
 
-            if (guess.equalsIgnoreCase(GET_ANOTHER_WORD )) {
+            if (guess.equalsIgnoreCase(GET_ANOTHER_WORD)) {
                 hangman.getAnotherWord();
                 continue;
             }
@@ -27,5 +40,11 @@ public class Runner {
 
         } while (!hangman.isWordGuessed()
                 && hangman.getHangmanState().getStage() < HangmanState.STAGE7.getStage());
+    }
+
+    private boolean playAgainFun(Scanner scanner) {
+        System.out.println(PLAY_AGAIN);
+        String response = scanner.nextLine();
+        return response.equalsIgnoreCase(PLAY_AGAIN_YES);
     }
 }
